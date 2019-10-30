cdfa96eef4fdfd53fdb9fbb4536a07186d3e12b3
DONGHOON IM <dongttang@gmail.com>
2019-10-30 23:05:45 +0900

    init

---
+++ clock.js
@@ -0,0 +1,26 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function pad(n, width) {
+  n = n + "";
+  return n.length >= width ? n : new Array(width - n.length + 1).join("0") + n;
+}
+
+function timeHandler() {
+  const currentDate = new Date();
+  const hour = currentDate.getHours();
+  const minute = currentDate.getMinutes();
+  const second = currentDate.getSeconds();
+  clockTitle.innerHTML = `
+  ${pad(hour, 2)}
+  : ${pad(minute, 2)}
+  : ${pad(second, 2)}`;
+  // :${pad(second, 2)}
+  // :${pad(millisecond, 3)}`;
+}
+
+function init() {
+  window.setInterval(timeHandler, 1);
+}
+
+init();
